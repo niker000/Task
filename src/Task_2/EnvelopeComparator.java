47009c878b1885ee64410a01238eafac8552fab8
niker000 <49456589+niker000@users.noreply.github.com>
2019-04-14 15:34:31 +0300

    Split code from Main into different classes. Added task_2.

--- src/Task_2/EnvelopeComparator.java
+++ src/Task_2/EnvelopeComparator.java
@@ -2,11 +2,11 @@ package Task_2;
 
 public class EnvelopeComparator {
     public static boolean compare(Envelope enveloper1, Envelope enveloper2) {
-        if (((enveloper1.getWidth() - enveloper2.getWidth()) > 0)
-                && (enveloper1.getHeight() - enveloper2.getHeight()) > 0) {
+        if (((enveloper1.getWidth() > enveloper2.getWidth()))
+                && (enveloper1.getHeight() > enveloper2.getHeight())) {
             return true;
-        } else if(((enveloper1.getHeight() - enveloper2.getWidth()) >0)
-                && ((enveloper1.getWidth() - enveloper2.getHeight()) > 0)) {
+        } else if(((enveloper1.getHeight() > enveloper2.getWidth()))
+                && ((enveloper1.getWidth() > enveloper2.getHeight()))) {
             return true;
         } else {
             return false;
