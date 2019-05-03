733f305b430532489cba947627230d2fefcc2eed
niker000 <49456589+niker000@users.noreply.github.com>
2019-05-03 03:19:40 +0300

    Added 5-8 tasks. For each task the main class was created.

--- src/task8/FibonachiMain.java
+++ src/task8/FibonachiMain.java
@@ -1,14 +1,15 @@
 package task8;
 
-import utils.ParameterParser;
 import utils.PrintHelper;
 
 public class FibonachiMain {
     public static void main(String[] args) {
         FibonachiNumbers fibonachiNumbers = new FibonachiNumbers();
+        long start = Integer.parseInt(args[0]);
+        long end = Integer.parseInt(args[1]);
         try {
-            PrintHelper.print(fibonachiNumbers.printNumbers(fibonachiNumbers.getFibonachiNumbers(ParameterParser.parseStrToInt(args[0]),
-                    ParameterParser.parseStrToInt(args[1]))));
+
+            PrintHelper.print(fibonachiNumbers.printNumbers(fibonachiNumbers.getFibonachiNumbers(start, end)));
         } catch (ArrayIndexOutOfBoundsException aioobe) {
             PrintHelper.print("Not not enough parameters");
         }
--- src/task8/FibonachiNumbers.java
+++ src/task8/FibonachiNumbers.java
@@ -1,11 +1,12 @@
 package task8;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FibonachiNumbers {
 
-    public String printNumbers(List<Long> numbers) {
+    public String printNumbers(List<BigInteger> numbers) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(numbers.get(0));
         for (int i = 1; i < numbers.size(); i++) {
@@ -14,37 +15,57 @@ public class FibonachiNumbers {
         return stringBuilder.toString();
     }
 
-    public List<Long> getFibonachiNumbers(long start, long end) {
-        ArrayList<Long> numbers = new ArrayList<>();
-        for (int i = 0; fibBinet(i) <= end; i++) {
-            long number = fibBinet(i);
-            if (number >= start) {
+    public List<BigInteger> getFibonachiNumbers(long start, long end) {
+
+        ArrayList<BigInteger> numbers = new ArrayList<>();
+        for (int i = 0; (fibonacci(i).compareTo(BigInteger.valueOf(end))== -1); i++) {
+            BigInteger number = fibonacci(i);
+            if (number.compareTo(BigInteger.valueOf(start))>=0) {
                 numbers.add(number);
-                // System.out.print(number + ", ");
             }
         }
         return numbers;
     }
 
-    public long fibBinet(long n) {
-        if (n > 70) {
-            return fibNumber(n);
-        }
-
-        double fi = (1 + Math.sqrt(5)) / 2;
-        return Math.round(Math.pow(fi, n) / Math.sqrt(5));
+    public BigInteger[][] matrixMultiplication(BigInteger[][] a, BigInteger[][] b) {
+        // [a00 * b00 + a01 * b10, a00 * b01 + a01 * b11]
+        // [a10 * b00 + a11 * b10, a10 * b01 + a11 * b11]
+        return new BigInteger[][]{
+                {a[0][0].multiply(b[0][0]).add(a[0][1].multiply(b[1][0])), a[0][0].multiply(b[0][1]).add(a[0][1].multiply(b[1][1]))},
+                {a[1][0].multiply(b[0][0]).add(a[1][1].multiply(b[1][0])), a[1][0].multiply(b[0][1]).add(a[1][1].multiply(b[1][1]))},
+        };
     }
 
-    public long fibNumber(long n) {
-        long a = 190392490709135L; // n = 70
-        long b = 117669030460994L;  //n = 69
-        long x;
-
-        for (long i = 0; i < n; i++) {
-            x = a + b;
-            a = b;
-            b = x;
+    // возведение матрицы размера 2 на 2 в степень n
+    public BigInteger[][] matrixPowerFast(BigInteger[][] a, int n) {
+        if (n == 0) {
+            // любая матрица в нулевой степени равна единичной матрице
+            return new BigInteger[][]{
+                    {BigInteger.ONE, BigInteger.ZERO},
+                    {BigInteger.ZERO, BigInteger.ONE}
+            };
+        } else if (n % 2 == 0) {
+            // a ^ (2k) = (a ^ 2) ^ k
+            return matrixPowerFast(matrixMultiplication(a, a), n / 2);
+        } else {
+            // a ^ (2k + 1) = (a) * (a ^ 2k)
+            return matrixMultiplication(matrixPowerFast(a, n - 1), a);
         }
-        return b;
+    }
+
+    // функция, возвращающая n-ое число Фибоначчи
+    public BigInteger fibonacci(int n) {
+        if (n == 0) {
+            return BigInteger.ZERO;
+        }
+
+        BigInteger[][] a = {
+                {BigInteger.ONE, BigInteger.ONE},
+                {BigInteger.ONE, BigInteger.ZERO}
+        };
+        BigInteger[][] powerOfA = matrixPowerFast(a, n - 1);
+        // nthFibonacci = powerOfA[0][0] * F_1 + powerOfA[0][0] * F_0 = powerOfA[0][0] * 1 + powerOfA[0][0] * 0
+        BigInteger nthFibonacci = powerOfA[0][0];
+        return nthFibonacci;
     }
 }
